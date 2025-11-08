e2a11ffd8a31c8dda3d5e6a436355c51b5091649
Giorgio Di Sarra <disarra02@yandex-team.ru>
2025-11-08 15:43:19 +0300

    Выставил вариант

--- src/main/java/ru/hse/de/vkcli/Main.java
+++ src/main/java/ru/hse/de/vkcli/Main.java
@@ -6,6 +6,6 @@ import ru.hse.de.vkcli.config.DummyIOCContainer;
 public class Main {
     public static void main(String[] args) {
         var staticService = DummyIOCContainer.createStatisticService();
-        new ReportFormatter().print(staticService.getMostPopularUserInCityReport(1), System.out);
+        new ReportFormatter().print(staticService.getMostPopularUserInCityReport(15), System.out);
     }
 }
